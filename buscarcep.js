557a0dacb95834f0c5573e454a44c5d37ffe2469
fsfreitas <ffreitas.ti@gmail.com>
2022-06-20 20:45:07 -0300

    Criando código JS responsável por consumir a api cep e exibir no layout html

---
+++ buscarcep.js
@@ -0,0 +1,45 @@
+var varcep = document.getElementById('campocep');
+varcep.addEventListener("blur", (evento)=>{
+    
+     //substituindo o traço por nada 
+     let pesquisa = varcep.value.replace("-","");  
+    
+     let option ={
+         method: 'get',
+         mode: 'cors',
+         cache: 'default'
+     }
+ 
+     //podendo usar assim também (com crase):  fetch(`https://viacep.com.br/ws/${pesquisa}/json/`, option);
+     fetch("https://viacep.com.br/ws/"+pesquisa+"/json/", option) 
+ 
+     //Se der certo
+     .then(response=>{response.json()//aqui já vou tratar em JSON
+         .then(dados=>{
+            //o código comentado abaixo serve para exibir no formato de objeto JSON
+            //console.log(dados)
+            document.getElementById('campoendereco').value = dados.logradouro;
+            document.getElementById('campobairro').value = dados.bairro;
+            document.getElementById('campocidade').value = dados.localidade;
+            document.getElementById('campoestado').value = dados.uf;
+         }
+         )
+     })
+     //se der errado
+     .catch(evento =>console.log('Algo deu errado: '+evento, message))
+});
+ 
+
+
+
+
+
+
+//função sempre chamada no carregamento da página e no botão APAGAR
+function resetarcampos(){
+        document.getElementById('campocep').value='';//apagando o campo CEP
+        document.getElementById('campocidade').value='';//apagando o campo cidade
+        document.getElementById('campoendereco').value='';//apagando o campo endereco
+        document.getElementById('campoestado').value='';//apagando o campo estado
+        document.getElementById('campobairro').value='';//apagando o campo bairro
+}
